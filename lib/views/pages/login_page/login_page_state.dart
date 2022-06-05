9b0a9063fb6e6b8ee45ef6bb9770906d2c104fb5
Simeon Dimitrov <simeon.dimitrov@athlonsofia.com>
2022-06-05 22:09:43 +0300

    Add login page state

---
+++ lib/views/pages/login_page/login_page_state.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPageState extends ChangeNotifier {
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  set isLoading(bool isLoading) {
+    _isLoading = isLoading;
+    notifyListeners();
+  }
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
