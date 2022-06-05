0f787313978eae02e3ca20e723c663166c8d9449
Simeon Dimitrov <simeon.dimitrov@athlonsofia.com>
2022-06-05 20:40:27 +0300

    Add welcome page

--- lib/views/pages/welcome_page/welcome_page.dart
+++ lib/views/pages/welcome_page/welcome_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:notr/views/pages/login_page/login_page.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: OutlinedButton(
+        key: const Key("loginPageButton"),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+          );
+        },
+        child: const Text("LOGIN WITH EMAIL"),
+      ),
+    );
   }
 }
