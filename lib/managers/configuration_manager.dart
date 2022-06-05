6d1899947e785e1cc06be8ff42ec9bcfe920c6c3
Simeon Dimitrov <simeon.dimitrov@athlonsofia.com>
2022-06-05 18:36:43 +0300

    Add support for late initialization

--- lib/managers/configuration_manager.dart
+++ lib/managers/configuration_manager.dart
@@ -2,12 +2,26 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:notr/models/fatal_error.dart';
+import 'package:notr/services/util/log_error.dart';
 
 class ConfigurationManager {
-  ConfigurationManager({
+  static ConfigurationManager? _instance;
+
+  factory ConfigurationManager({
+    required FirebaseOptions firebaseOptions,
+    String? firebaseAppName,
+  }) {
+    return _instance ??= ConfigurationManager._(
+      firebaseOptions: firebaseOptions,
+      firebaseAppName: firebaseAppName,
+    );
+  }
+
+  ConfigurationManager._({
     required this.firebaseOptions,
     this.firebaseAppName,
   });
@@ -15,32 +29,47 @@ class ConfigurationManager {
   final String? firebaseAppName;
   final FirebaseOptions firebaseOptions;
 
+  bool _isInitialized = false;
+
   void configure(Widget widget) {
     WidgetsFlutterBinding.ensureInitialized();
 
     runZonedGuarded(
       () async {
-        await initialize();
-
         runApp(widget);
       },
       (error, stack) async {
+        if (!_isInitialized && kDebugMode) {
+          debugPrint(
+            "Application is not initialized. Call ConfigurationManager.initialize().",
+          );
+        }
+
         if (error is FatalError) _exitApp();
+
+        logError(error, stack);
       },
     );
   }
 
-  Future<bool> initialize() async {
+  Future<void> initialize({
+    bool allowDataCollection = true,
+  }) async {
     try {
       await Firebase.initializeApp(
         name: firebaseAppName,
         options: firebaseOptions,
       );
 
+      // Disable data collection in debug mode or user disables it.
+      if (kDebugMode || !allowDataCollection) {
+        FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+      }
+
       FlutterError.onError =
           FirebaseCrashlytics.instance.recordFlutterFatalError;
 
-      return true;
+      _isInitialized = true;
     } catch (error, stack) {
       throw FatalError(error, stack);
     }
