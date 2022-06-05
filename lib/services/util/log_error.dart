5c05d8df1ffb0aacbb01928d5d200639ab053b9f
Simeon Dimitrov <simeon.dimitrov@athlonsofia.com>
2022-06-05 18:39:19 +0300

    Add crashlytics error log

--- lib/services/util/log_error.dart
+++ lib/services/util/log_error.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
+import 'package:notr/models/fatal_error.dart';
 
 void logError(Object error, StackTrace stack) {
   debugPrint('-------------------- ERROR LOG -----------------------');
   debugPrint('ERROR: $error');
   debugPrintStack(stackTrace: stack);
   debugPrint('----------------------- END --------------------------');
+
+  if (!kDebugMode) {
+    FirebaseCrashlytics.instance.recordError(
+      error,
+      stack,
+      fatal: error is FatalError,
+    );
+  }
 }
