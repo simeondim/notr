4dae5d3482a7a9bdecc4b7c73a4a5111271e3285
Simeon Dimitrov <simeon.dimitrov@athlonsofia.com>
2022-06-06 19:14:48 +0300

    Move test file to correct directory

--- lib/managers/firebase_auth_failure_manager.dart
+++ lib/managers/firebase_auth_failure_manager.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:notr/models/failure_manager.dart';
 import 'package:notr/models/failures/failure.dart';
@@ -14,6 +15,11 @@ class FirebaseAuthFailureManager implements FailureManager {
   /// Implements possible errors from [FirebaseAuth.instance.signInWithEmailAndPassword]
   @visibleForTesting
   Failure? getSignInWithEmailAndPasswordFailure(Object error) {
+    if (error is! FirebaseAuthException) return null;
+
+    if (error.code == '') {
+      return null;
+    }
     return null;
   }
 }
