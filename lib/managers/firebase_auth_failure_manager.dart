3642f61a025e717d6f1005ebfcb1ef9587807242
Simeon Dimitrov <simeon.dimitrov@athlonsofia.com>
2022-06-06 19:18:30 +0300

    Add firebase authentication failure manager

--- lib/managers/firebase_auth_failure_manager.dart
+++ lib/managers/firebase_auth_failure_manager.dart
@@ -2,6 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:notr/models/failure_manager.dart';
 import 'package:notr/models/failures/failure.dart';
+import 'package:notr/models/failures/invalid_email.dart';
+import 'package:notr/models/failures/invalid_password.dart';
+import 'package:notr/models/failures/not_available.dart';
+import 'package:notr/models/failures/not_valid.dart';
+import 'package:notr/models/failures/unknown_failure.dart';
 
 class FirebaseAuthFailureManager implements FailureManager {
   @override
@@ -17,9 +22,22 @@ class FirebaseAuthFailureManager implements FailureManager {
   Failure? getSignInWithEmailAndPasswordFailure(Object error) {
     if (error is! FirebaseAuthException) return null;
 
-    if (error.code == '') {
-      return null;
+    if (error.code == 'invalid-email') {
+      return const InvalidEmail(NotValid());
     }
+
+    if (error.code == 'user-disabled') {
+      return const InvalidEmail(NotAvailable());
+    }
+
+    if (error.code == 'user-not-found') {
+      return const InvalidEmail(UnknownFailure());
+    }
+
+    if (error.code == 'wrong-password') {
+      return const InvalidPassword(NotValid());
+    }
+
     return null;
   }
 }
