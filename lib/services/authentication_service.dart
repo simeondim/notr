7dd5ed29d7b1cc252698d4796a29feedf12a4181
Simeon Dimitrov <simeon.dimitrov@athlonsofia.com>
2022-06-06 08:52:01 +0300

    Add sign in with email service

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -2,8 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:notr/models/either.dart';
 import 'package:notr/models/email_and_password_credentials.dart';
 import 'package:notr/models/failures/failure.dart';
-import 'package:notr/models/failures/unknown_failure.dart';
 import 'package:notr/repository/authentication_repository.dart';
+import 'package:notr/services/util/error_handler.dart';
 
 class AuthenticationService {
   const AuthenticationService({
@@ -15,6 +15,11 @@ class AuthenticationService {
   Future<Either<Failure, UserCredential>> signInWithEmailAndPassword(
     EmailAndPasswordCredentials credentials,
   ) async {
-    return Left(const UnknownFailure());
+    return errorHandler(() async {
+      final failure = credentials.validate();
+      if (failure != null) throw failure;
+
+      return await _authRepo.signInWithEmailAndPassword(credentials);
+    });
   }
 }
