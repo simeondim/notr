d1554acfc38617203e895ae54ffaf364ba17457f
Simeon Dimitrov <simeon.dimitrov93@gmail.com>
2022-06-07 08:19:47 +0300

    Add auth layer

--- lib/views/widgets/authentication_layer/authentication_layer.dart
+++ lib/views/widgets/authentication_layer/authentication_layer.dart
@@ -1,21 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:notr/services/authentication_service.dart';
 
 class AuthenticationLayer extends StatefulWidget {
   const AuthenticationLayer({
+    required this.unauthenticated,
+    required this.authenticated,
     this.authService = const AuthenticationService(),
     super.key,
   });
 
   final AuthenticationService authService;
+  final Widget authenticated;
+  final Widget unauthenticated;
 
   @override
   State<AuthenticationLayer> createState() => _AuthenticationLayerState();
 }
 
 class _AuthenticationLayerState extends State<AuthenticationLayer> {
+  User? user;
+
+  @override
+  void initState() {
+    widget.authService.listenForAuthStateChanges(_onAuthChanged);
+    super.initState();
+  }
+
+  void _onAuthChanged(User? newUser) => setState(() => user = newUser);
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    if (user != null) {
+      return SizedBox(
+        key: const Key('authenticatedWidget'),
+        child: widget.authenticated,
+      );
+    }
+
+    return SizedBox(
+      key: const Key("unauthenticatedWidget"),
+      child: widget.unauthenticated,
+    );
   }
 }
